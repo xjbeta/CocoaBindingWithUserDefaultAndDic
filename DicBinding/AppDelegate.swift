6b644bca34b3846fc63876710d5f8c2e32bf9003
xjbeta <xjbeta.tiny@gmail.com>
2019-02-16 16:45:25 +0800

    Initial Commit

---
+++ DicBinding/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DicBinding
+//
+//  Created by xjbeta on 2019/2/16.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
