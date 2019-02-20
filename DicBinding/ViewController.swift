1449ad5645196ca3c04c7505261db57afd4981ec
xjbeta <xjbeta.tiny@gmail.com>
2019-02-20 18:52:18 +0800

    Cocoa Binding with dictionary in UserDefault.

--- DicBinding/ViewController.swift
+++ DicBinding/ViewController.swift
@@ -10,10 +10,32 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    let savedDic = "savedDic"
+    
+    @IBAction func printDic(_ sender: Any) {
+        if let dic = UserDefaults.standard.dictionary(forKey: savedDic) {
+            print(dic)
+        }
+    }
+    
+    @IBAction func updateDic(_ sender: Any) {
+        if var dic = UserDefaults.standard.dictionary(forKey: savedDic),
+            let str = dic["testValue2"] as? String {
+            dic["testValue2"] = str + "+1s"
+            UserDefaults.standard.set(dic, forKey: savedDic)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let dic = UserDefaults.standard.dictionary(forKey: savedDic) {
+            print(dic)
+        } else {
+            let d = ["testValue1": true,
+                     "testValue2": "someString"] as [String : Any]
+            UserDefaults.standard.set(d, forKey: savedDic)
+        }
     }
 
     override var representedObject: Any? {
